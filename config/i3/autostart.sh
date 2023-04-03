ed75ebbe3da59ab9542ff5f1b36962be67c820cd
Sulav Baral <sulavbaral58@gmail.com>
2023-04-03 14:46:46 +0545

    new configs

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -2,18 +2,19 @@
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-# nitrogen --restore; sleep 1; 
+nitrogen --restore; sleep 1; 
 
 # Add this to your .xinitrc or whatever file starts programs on startup.
 # -R restores the last colorscheme that was in use.
 wal -R
 
-/usr/local/bin/picom -b&
 numlockx on &
 nm-applet &
 volumeicon &
 dunst -config ~/.config/dunst/dunstrc &
 # blueman-applet &
+dbus-launch&
 
 # sxhkd
 sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
+
