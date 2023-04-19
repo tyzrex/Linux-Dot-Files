0601ec167f50b70d48c2737aac2a8857c116ee13
Sulav Baral <sulavbaral58@gmail.com>
2023-04-19 16:10:39 +0545

    polybar i3

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -2,10 +2,6 @@
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-<<<<<<< HEAD
-=======
-nitrogen --restore; sleep 1; 
->>>>>>> 19918f47c527e4f04a3c8ea4a1835fb8a7f9f9ab
 
 # Add this to your .xinitrc or whatever file starts programs on startup.
 # -R restores the last colorscheme that was in use.
@@ -14,16 +10,10 @@ wal -R
 numlockx on &
 nm-applet &
 volumeicon &
-<<<<<<< HEAD
 dunst -config ~/.config/dunst-back/dunstrc &
 # blueman-applet &
 dbus-launch&
 ~/.config/polybar/launch.sh&
-=======
-dunst -config ~/.config/dunst/dunstrc &
-# blueman-applet &
-dbus-launch&
->>>>>>> 19918f47c527e4f04a3c8ea4a1835fb8a7f9f9ab
 
 # sxhkd
 sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
